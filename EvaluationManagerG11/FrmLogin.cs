ef37cd13c364327c5325d21e120d4f91d26a5117
Dominik Černjević <dcernjevi21@foi.hr>
2023-04-25 17:49:06 +0200

    Dorađena forma za login

--- EvaluationManagerG11/FrmLogin.cs
+++ EvaluationManagerG11/FrmLogin.cs
@@ -10,11 +10,19 @@ using System.Windows.Forms;
 
 namespace EvaluationManagerG11
 {
-    public partial class Form1 : Form
+    public partial class frmLogin : Form
     {
-        public Form1()
+        public frmLogin()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void txtUsername_TextChanged(object sender, EventArgs e) {
+
         }
     }
 }
